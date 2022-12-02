e66323668118ef13d5541215acee3a7e67d74770
michaelporter <michael@teepublic.com>
2022-12-02 16:38:13 -0500

    adds nav to top of index

--- advent/lib/advent/solution/day_1.ex
+++ advent/lib/advent/solution/day_1.ex
@@ -1,15 +1,7 @@
 defmodule Advent.Solution.Day1 do
-  def parse_response_body(body) do
-    String.trim_trailing(body)
-    |> String.split("\n\n")
-    |> Enum.map(fn elf ->
-      String.split(elf, "\n")
-      |> Enum.map(fn item -> String.to_integer(item) end)
-    end)
-  end
 
   def part_one do
-    elf_backpacks = Advent.InputFetcher.fetch_for_day(1, &parse_response_body/1)
+    elf_backpacks = get_problem_input
 
     Enum.reduce(elf_backpacks, 0, fn elf_backpack, last_highest ->
       Enum.reduce(elf_backpack, 0, fn item, calories -> item + calories end)
@@ -17,8 +9,14 @@ defmodule Advent.Solution.Day1 do
     end)
   end
 
+  defp highest_one(first, second) do
+    Enum.sort([first, second]) |> List.last
+  end
+
+  ###
+
   def part_two do
-    elf_backpacks = Advent.InputFetcher.fetch_for_day(1, &parse_response_body/1)
+    elf_backpacks = get_problem_input
 
     Enum.reduce(elf_backpacks, [], fn elf_backpack, top_three ->
       Enum.reduce(elf_backpack, 0, fn item, calories -> item + calories end)
@@ -27,14 +25,26 @@ defmodule Advent.Solution.Day1 do
     |> Enum.reduce(fn elf, total -> elf + total end)
   end
 
-  defp highest_one(first, second) do
-    Enum.sort([first, second]) |> List.last
-  end
-
   defp highest_three(contender, current_set \\ []) do
     case current_set do
       [a, b, c] -> [contender | current_set] |> Enum.sort |> Enum.slice(1, 3)
       _ -> [contender | current_set]
     end
   end
+
+  ###
+
+  defp parse_response_body(body) do
+    String.trim_trailing(body)
+    |> String.split("\n\n")
+    |> Enum.map(fn elf ->
+      String.split(elf, "\n")
+      |> Enum.map(fn item -> String.to_integer(item) end)
+    end)
+  end
+
+  defp get_problem_input do
+    Advent.InputFetcher.fetch_for_day(1, &parse_response_body/1)
+  end
+
 end
--- advent/lib/advent/solution/day_2.ex
+++ advent/lib/advent/solution/day_2.ex
@@ -1,7 +1,19 @@
-defmodule Advent.Solution.DayTwo do
-  def part_one(input) do
+defmodule Advent.Solution.Day2 do
+
+  def part_one do
   end
 
-  def part_two(input) do
+  ###
+
+  def part_two do
+  end
+
+  ###
+
+  defp parse_response_body(body) do
+  end
+
+  defp get_problem_input do
+    Advent.InputFetcher.fetch_for_day(2, &parse_response_body/1)
   end
 end
