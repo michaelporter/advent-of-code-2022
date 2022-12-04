d491079c9f917d15e871ff8c8971ebb6ffa24624
michaelporter <michael@teepublic.com>
2022-12-04 01:35:45 -0500

    more day 4 cleanup

--- advent/lib/advent/solution/day_4.ex
+++ advent/lib/advent/solution/day_4.ex
@@ -1,16 +1,13 @@
 defmodule Advent.Solution.Day4 do
 
   def part_one do
-    Enum.reduce(get_problem_input, 0, fn pair, overlapping ->
-      [[start1, end1],[start2, end2]] = pair
-
-      range1 = Enum.to_list(start1..end1)
-      range2 = Enum.to_list(start2..end2)
+    Enum.reduce(get_problem_input, 0, fn assignment_pair, overlapping ->
+      [assignment1, assignment2] = assignment_pair
 
       cond do
-        Enum.empty?(range1 -- range2) ->
+        Enum.empty?(assignment1 -- assignment2) ->
           overlapping = overlapping + 1
-        Enum.empty?(range2 -- range1) ->
+        Enum.empty?(assignment2 -- assignment1) ->
           overlapping = overlapping + 1
         true ->
           overlapping
@@ -21,16 +18,13 @@ defmodule Advent.Solution.Day4 do
   ###
 
   def part_two do
-    Enum.reduce(get_problem_input, 0, fn pair, overlapping ->
-      [[start1, end1], [start2, end2]] = pair
-
-      range1 = Enum.to_list(start1..end1)
-      range2 = Enum.to_list(start2..end2)
+    Enum.reduce(get_problem_input, 0, fn assignment_pair, overlapping ->
+      [assignment1, assignment2] = assignment_pair
 
       cond do
-        length(range1 -- range2) < length(range1) ->
+        length(assignment1 -- assignment2) < length(assignment1) ->
           overlapping = overlapping + 1
-        length(range2 -- range1) < length(range2) ->
+        length(assignment2 -- assignment1) < length(assignment2) ->
           overlapping = overlapping + 1
         true ->
           overlapping
@@ -49,6 +43,13 @@ defmodule Advent.Solution.Day4 do
       Enum.map(pair, fn p ->
         String.split(p, "-") |> Enum.map(fn num -> String.to_integer(num) end)
       end)
+    end)
+    |> Enum.map(fn pair ->
+      [[start1, end1],[start2, end2]] = pair
+      [
+        Enum.to_list(start1..end1),
+        Enum.to_list(start2..end2)
+      ]
     end)
   end
 
