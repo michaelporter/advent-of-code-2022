552a658d0bf7e58bfa8715ebe5a2406fa5fce705
michaelporter <michael@teepublic.com>
2022-12-18 22:42:13 -0600

    removes unneeded func from day 11

---
+++ advent/lib/advent/solution/day_12.ex
@@ -0,0 +1,19 @@
+defmodule Advent.Solution.Day12 do
+
+  def part_one do
+  end
+
+  def part_two do
+  end
+
+  ###
+
+  defp parse_response_body do
+  end
+
+
+  # I didn't parse the input this time; there is no honor here
+  defp get_problem_input do
+    Advent.InputFetcher.fetch_for_day(12, &parse_response_body/1)
+  end
+end
